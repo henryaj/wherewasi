8d4e87e0ec517fd02026eed9d0e1aea979eda743
Henry Stanley <henry@henrystanley.com>
2014-09-09 15:33:58 +0100

    Extracted controllers into separate folder

--- app/server.rb
+++ app/server.rb
@@ -8,70 +8,13 @@ require_relative './models/user'
 require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
 
+require_relative 'controllers/application'
+require_relative './controllers/links'
+require_relative './controllers/sessions'
+require_relative './controllers/tags'
+require_relative './controllers/users'
+
 enable :sessions
 set :session_secret, 'super secret'
-
-
-
-get '/' do 
-	@links = Link.all
-	erb :index
-end
-
-post '/links' do 
-	url = params["url"]
-	title = params["title"]
-	tags = params["tags"].split(" ").map do |tag|
-		Tag.first_or_create(:text => tag)
-	end
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to('/')
-end
-
-get '/tags/:text' do 
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end 
-
-post '/users' do
-	@user = User.new(:email => params[:email],
-				:password => params[:password],
-				:password_confirmation => params[:password_confirmation])
-	if @user.save
-		session[:user_id] = @user.id
-		redirect to('/')
-	else
-		flash.now[:errors] = @user.errors.full_messages
-		erb :"users/new"
-	end
-end
-
-get '/users/new' do
-	@user = User.new
-	erb :"users/new"
-end
-
-get '/sessions/new' do 
-	erb :"sessions/new"
-end
-
-post '/sessions' do 
-	email, password = params[:email], params[:password]
-	user = User.authenticate(email, password)
-	if user
-		session[:user_id] = user.id
-		redirect to("/")
-	else
-		flash[:error] = ["The email or password is incorrect"]
-		erb :"sessions/new"
-	end
-end
-
-delete '/sessions' do
-	session[:user_id] = nil
-	flash[:notice] = "Good bye!"
-end
-
 
 # run Sinatra::Application.run!
